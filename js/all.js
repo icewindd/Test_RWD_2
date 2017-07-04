f23419307da919045d5ea41a29949b73b114da74
icewindd <achiang1225@gmail.com>
2017-07-04 20:11:51 +0800

    updata jquery and css effect

--- js/all.js
+++ js/all.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	$('.view h2').addClass('active');
+	$('.view h3').addClass('active');
+$(function () {
+  $(window).scroll(function () {
+    	var scrollVal = $(this).scrollTop();
+		var headerH = $('.header').position().top;
+		var viewH = $('.view').position().top;
+		var contentH = $('.content').position().top;
+		var introH = $('.intro').position().top;
+		console.log(scrollVal);
+		console.log(introH);
+		console.log('---------');
+		if(  scrollVal > contentH/2){
+		$('.animateJsUp').addClass('active');
+		}
+		if(scrollVal > introH-100){
+			$('.animateJsDown').addClass('active');
+		}
+	});
+});
 
 	$(function(){
 		$('.js-bar').click(function(e){
@@ -9,6 +29,5 @@ $(document).ready(function() {
 			e.preventDefault();
 		$('.js-menu').slideUp();
 		});
-
 	});
 });
